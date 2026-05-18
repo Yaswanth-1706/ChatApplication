03819810e562463c66aa2df11a76415830ea5d91
Yaswanth <yaswanthgurram17@gmail.com>
2026-05-18 16:02:18 +0530

    latest changes made

--- frontend/frontend/src/compoents/Editprofile.jsx
+++ frontend/frontend/src/compoents/Editprofile.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
-import './editProfile.css';
+import './editprofile.css';
 
 const Editprofile = () => {
     const location = useLocation();
