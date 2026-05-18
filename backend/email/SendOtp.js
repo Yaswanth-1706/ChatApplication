d31b237a92d926f6787ade9b5ce8359656a8013a
Yaswanth <yaswanthgurram17@gmail.com>
2026-05-18 18:20:32 +0530

    latest commit

--- backend/email/SendOtp.js
+++ backend/email/SendOtp.js
@@ -1,13 +1,11 @@
 const nodemailer=require("nodemailer")
 const dotEnv=require("dotenv")
 dotEnv.config()
-const transporter=nodemailer.createTransport({
-    host:"smtp.gmail.com",
-    port:587,
-    secure:false,
+const transporter = nodemailer.createTransport({
+    service:"gmail",
     auth:{
-          user:process.env.Email_User,
-          pass:process.env.Email_Pass
+        user:process.env.Email_User,
+        pass:process.env.Email_Pass
     }
 })
 exports.sendOtpEmail=async(email,otp)=>{
