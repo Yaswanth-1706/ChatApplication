075e3078e00c1c7946e74bed9b86452ab835d105
Yaswanth <yaswanthgurram17@gmail.com>
2026-05-19 15:27:47 +0530

    j

--- backend/middelwares/imageMiddleware.js
+++ backend/middelwares/imageMiddleware.js
@@ -45,10 +45,6 @@ const fileFilter = (req,file,cb)=>{
 }
 const upload = multer({
    storage,
-   fileFilter,
-
-   limits: {
-      fileSize: 50 * 1024 * 1024 // 50MB
-   }
+   fileFilter
 })
 module.exports = upload
