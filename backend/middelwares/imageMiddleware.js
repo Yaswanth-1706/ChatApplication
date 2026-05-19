4eb534d3787d02b8f13fc5458c94f6c1e75b9180
Yaswanth <yaswanthgurram17@gmail.com>
2026-05-19 15:17:53 +0530

    commit

--- backend/middelwares/imageMiddleware.js
+++ backend/middelwares/imageMiddleware.js
@@ -1,47 +1,50 @@
 const multer = require("multer")
 
 const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/")
+  },
 
-   destination:(req,file,cb)=>{
-
-      cb(null,"uploads/")
-   },
-
-   filename:(req,file,cb)=>{
-
-      cb(null,Date.now() + "-" + file.originalname)
-   }
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname)
+  }
 })
 
-const fileFilter = (req,file,cb)=>{
+const fileFilter = (req, file, cb) => {
 
-   const allowedTypes = [
+  const allowedTypes = [
 
-      "image/png",
-      "image/jpeg",
-      "image/jpg",
-      "image/webp",
+    "image/png",
+    "image/jpeg",
+    "image/jpg",
+    "image/webp",
 
-      "video/mp4",
-      "video/webm",
-      "video/ogg",
+    "video/mp4",
+    "video/webm",
+    "video/ogg",
 
-      "audio/mp3",
-      "audio/mpeg",
-      "audio/wav",
-      "audio/ogg",
+    "audio/mp3",
+    "audio/mpeg",
+    "audio/wav",
+    "audio/ogg",
 
-      "application/pdf"
-   ]
+    "application/pdf"
+  ]
 
-   if(allowedTypes.includes(file.mimetype)){
-
-      cb(null,true)
-
-   }else{
-
-      cb(new Error("Unsupported file type"),false )
-   }
+  if (allowedTypes.includes(file.mimetype)) {
+    cb(null, true)
+  } else {
+    cb(new Error("Unsupported file type"), false)
+  }
 }
-const upload = multer({storage,fileFilter})
+
+const upload = multer({
+  storage,
+  fileFilter,
+
+  limits: {
+    fileSize: 50 * 1024 * 1024 // 50MB
+  }
+})
+
 module.exports = upload
