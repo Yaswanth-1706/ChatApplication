f9665ed927a69d7342cfd55f20a567a242a00070
Yaswanth <yaswanthgurram17@gmail.com>
2026-05-19 10:03:01 +0530

    small changes were made

--- backend/email/SendOtp.js
+++ backend/email/SendOtp.js
@@ -1,32 +1,35 @@
-const { TransactionalEmailsApi, SendSmtpEmail, ApiClient } = require("@getbrevo/brevo");
+const axios = require("axios");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const apiClient = ApiClient.instance;
-apiClient.authentications["api-key"].apiKey = process.env.BREVO_API_KEY;
-
-const apiInstance = new TransactionalEmailsApi();
-
 exports.sendOtpEmail = async (email, otp) => {
     try {
-        const sendSmtpEmail = new SendSmtpEmail();
+        const response = await axios.post(
+            "https://api.brevo.com/v3/smtp/email",
+            {
+                sender: {
+                    name: "chatApp",
+                    email: process.env.BREVO_SENDER_EMAIL
+                },
+                to: [{ email: email }],
+                subject: "Your OTP for Email Verification",
+                htmlContent: `
+                    <h2>Your OTP is: ${otp}</h2>
+                    <p>OTP valid for 5 minutes</p>
+                `
+            },
+            {
+                headers: {
+                    "api-key": process.env.BREVO_API_KEY,
+                    "Content-Type": "application/json"
+                }
+            }
+        );
 
-        sendSmtpEmail.subject = "Your OTP for Email Verification";
-        sendSmtpEmail.htmlContent = `
-            <h2>Your OTP is: ${otp}</h2>
-            <p>OTP valid for 5 minutes</p>
-        `;
-        sendSmtpEmail.sender = {
-            name: "chatApp",
-            email: process.env.BREVO_SENDER_EMAIL
-        };
-        sendSmtpEmail.to = [{ email: email }];
-
-        const data = await apiInstance.sendTransacEmail(sendSmtpEmail);
-        console.log("Email sent:", data);
+        console.log("Email sent:", response.data);
 
     } catch (err) {
-        console.log("FULL MAIL ERROR:", err);
+        console.log("FULL MAIL ERROR:", err.response?.data || err.message);
         throw err;
     }
 };
