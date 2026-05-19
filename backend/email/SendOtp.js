36a0d1a2a28635ed6b484f7e28c36167dfd8530b
Yaswanth <yaswanthgurram17@gmail.com>
2026-05-19 07:21:56 +0530

    latest

--- backend/email/SendOtp.js
+++ backend/email/SendOtp.js
@@ -5,22 +5,19 @@ dotenv.config()
 
 // ================= TRANSPORTER =================
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+
     auth: {
         user: process.env.Email_User,
         pass: process.env.Email_Pass
-    }
-})
+    },
 
-// ================= VERIFY CONNECTION =================
-transporter.verify((err, success) => {
-    if (err) {
-        console.log("Email transporter error:", err.message)
-    } else {
-        console.log("Email server is ready")
-    }
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000
 })
-
 // ================= SEND OTP EMAIL =================
 exports.sendOtpEmail = async (email, otp) => {
     try {
