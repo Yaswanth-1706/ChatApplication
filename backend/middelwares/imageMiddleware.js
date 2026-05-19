321633b2bc76bab8b60986013e4f53875d035ee4
Yaswanth <yaswanthgurram17@gmail.com>
2026-05-19 13:52:03 +0530

    commit

--- backend/middelwares/imageMiddleware.js
+++ backend/middelwares/imageMiddleware.js
@@ -1,22 +1,15 @@
 const multer = require("multer")
-const fs = require("fs")
-
-if (!fs.existsSync("uploads")) {
-   fs.mkdirSync("uploads")
-}
 
 const storage = multer.diskStorage({
 
    destination:(req,file,cb)=>{
+
       cb(null,"uploads/")
    },
 
    filename:(req,file,cb)=>{
 
-      const uniqueName =
-         Date.now() + "-" + file.originalname.replace(/\s+/g,"-")
-
-      cb(null,uniqueName)
+      cb(null,Date.now() + "-" + file.originalname)
    }
 })
 
@@ -47,19 +40,8 @@ const fileFilter = (req,file,cb)=>{
 
    }else{
 
-      cb(new Error("Unsupported file type"),false)
+      cb(new Error("Unsupported file type"),false )
    }
 }
-
-const upload = multer({
-
-   storage,
-
-   fileFilter,
-
-   limits:{
-      fileSize:50 * 1024 * 1024
-   }
-})
-
+const upload = multer({storage,fileFilter})
 module.exports = upload
