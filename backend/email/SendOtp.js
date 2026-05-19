151b92be4c59ee164d64d96eb824892caef0a582
Yaswanth <yaswanthgurram17@gmail.com>
2026-05-19 09:59:58 +0530

    cide modefied

--- backend/email/SendOtp.js
+++ backend/email/SendOtp.js
@@ -1,21 +1,23 @@
-const Brevo = require("@getbrevo/brevo");
+const { TransactionalEmailsApi, SendSmtpEmail, ApiClient } = require("@getbrevo/brevo");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const apiInstance = new Brevo.TransactionalEmailsApi();
-apiInstance.authentications["apiKey"].apiKey = process.env.BREVO_API_KEY;
+const apiClient = ApiClient.instance;
+apiClient.authentications["api-key"].apiKey = process.env.BREVO_API_KEY;
+
+const apiInstance = new TransactionalEmailsApi();
 
 exports.sendOtpEmail = async (email, otp) => {
     try {
-        const sendSmtpEmail = new Brevo.SendSmtpEmail();
+        const sendSmtpEmail = new SendSmtpEmail();
 
         sendSmtpEmail.subject = "Your OTP for Email Verification";
         sendSmtpEmail.htmlContent = `
             <h2>Your OTP is: ${otp}</h2>
             <p>OTP valid for 5 minutes</p>
         `;
-        sendSmtpEmail.sender = { 
-            name: "chatApp", 
+        sendSmtpEmail.sender = {
+            name: "chatApp",
             email: process.env.BREVO_SENDER_EMAIL
         };
         sendSmtpEmail.to = [{ email: email }];
